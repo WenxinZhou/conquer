1b4276f140686a94f67d46f1af4800532f737811
Wenxin Zhou <wez243@mcsadmins-iMac.local>
2021-09-01 23:40:28 -0700

    setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-install_requires = ['numpy', 'scipy.stats']
+install_requires = ['numpy', 'scipy']
 
 setup(
     version="1.0",
