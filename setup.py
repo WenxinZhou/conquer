d3dac54948ea28e3d7f8f3b43f96d8bd0bf1dedb
Wenxin Zhou <wenxinz@131-193-209-218.cba.uic.edu>
2023-11-22 16:44:15 -0600

    conquer v2.0

--- setup.py
+++ setup.py
@@ -6,30 +6,23 @@ with open("README.md", "r", encoding="utf-8") as fh:
 install_requires = ['numpy', 'scipy', 'cvxopt']
 
 setup(
-    version="1.0",
-
-    #find all packages using the following command.
-    packages=find_packages(),
     name='conquer',
-    scripts=[],
-    
-    package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
-        # And include any *.msg files found in the 'hello' package, too:
-        'hello': ['*.msg'],
-    },
-
+    version="2.0",
+    packages=find_packages(),
     author="Wenxin Zhou",
     author_email="wenxinz@uic.edu",
     description="Convolution Smoothed Quantile Regression",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    keywords="conquer",
-
-    license='GPL-3.0',
+    url="https://github.com/WenxinZhou/conquer",  # replace with the URL of your project
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+        '': ['*.txt', '*.rst'],
+        'hello': ['*.msg'],
+    },
     install_requires=install_requires,
-    test_suite='nose.collector',
-    tests_require=['nose'],
-    zip_safe=False
 )
