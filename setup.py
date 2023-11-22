1e26fc370d34117de09a95b075de07d54975d48e
Wenxin Zhou <wenxinz@131-193-209-218.cba.uic.edu>
2023-11-22 15:06:11 -0600

    package updated

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-install_requires = ['numpy', 'scipy']
+install_requires = ['numpy', 'scipy', 'cvxopt']
 
 setup(
     version="1.0",
 
     #find all packages using the following command.
-    packages=["conquer"],
+    packages=find_packages(),
     name='conquer',
     scripts=[],
     
